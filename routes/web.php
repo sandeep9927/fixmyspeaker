376d56c77f254481effaf5bbf0a4feaf98cd1397
sandeep554 <sandeep.saini@kelltontech.com>
2021-07-01 16:25:56 +0530

    change config

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('fixe-your-speakers','FixSpeakerController@index');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/','FixSpeakerController@index');
